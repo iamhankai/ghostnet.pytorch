d0c517737832053164b1095e58b591157105581c
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-04-10 12:24:36 -0700

    Update ghost_net.py
    Fix for https://github.com/iamhankai/ghostnet.pytorch/issues/21

--- ghost_net.py
+++ ghost_net.py
@@ -98,7 +98,7 @@ class GhostBottleneck(nn.Module):
             self.shortcut = nn.Sequential()
         else:
             self.shortcut = nn.Sequential(
-                depthwise_conv(inp, inp, 3, stride, relu=True),
+                depthwise_conv(inp, inp, 3, stride, relu=False),
                 nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
                 nn.BatchNorm2d(oup),
             )
